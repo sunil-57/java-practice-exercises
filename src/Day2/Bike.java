9c68c91bc04a07bf57d3b68c656f3996d9b6ad98
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-18 11:46:43 +0545

    added attributes to class

---
+++ src/Day2/Bike.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class Bike {
+    //TODO bike entity ko barema chahine data k k ho?
+    // bike ko nam, model, engine number, baneko date, color
+    String bike_name;
+    int bike_model;
+    double engine_number;
+    String manufactured_date;
+    String color;
+    //entity ko data lai variables haru use garera rakhinxa
+
+    //TODO bike ko behaviour k k represent garne??
+    //
+}
