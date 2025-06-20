a2983aaef3b90b25ba3d3624ac5e483bd575f0ce
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-20 12:13:40 +0545

    Student class completed

---
+++ src/Day3/Student.java
@@ -0,0 +1,32 @@
+package Day3;
+
+import java.sql.SQLOutput;
+
+public class Student {
+    private String id;
+    public String name;
+    private int marks;
+
+    public Student(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void changeGrade(int marksGivenByUser){
+        this.marks = marksGivenByUser;
+    }
+
+    public String getGrade(){
+        int marks = this.marks;
+        if(marks < 0){
+            return "Invalid Marks";
+        }else{
+            if(marks>70){
+                return "A+";
+            }else{
+               return "Failed";
+            }
+        }
+    }
+
+}
